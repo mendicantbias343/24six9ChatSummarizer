186da52796b2f5225a297d4c60cf227c764193c5
Rameez Kakodker <49229745+medicantBias117@users.noreply.github.com>
2023-07-31 12:11:17 +0400

    Nuxt Links Replace A Hrefs

--- frontend/pages/summary/[date].vue
+++ frontend/pages/summary/[date].vue
@@ -3,11 +3,11 @@
       <div
         class="date-navigation text-md text-primary-focus flex-row flex justify-around w-full items-stretch text-2xl sticky top-0 bg-base-100 z-20 py-3"
       >
-        <div  v-bind:class="previous_date?'next cursor-pointer':'next cursor-pointer grayscale'"><a v-bind:href="previous_date">⏮️</a></div>
+        <div  v-bind:class="previous_date?'next cursor-pointer':'next cursor-pointer grayscale'"><NuxtLink v-bind:to="previous_date">⏮️</NuxtLink></div>
         <div class="current-date w-4/5 text-center text-lg">
           {{ formattedDate}}
         </div>
-        <div   v-bind:class="next_date ? 'next cursor-pointer' : 'next cursor-pointer grayscale'"><a v-bind:href="next_date">⏭️</a></div>
+        <div   v-bind:class="next_date ? 'next cursor-pointer' : 'next cursor-pointer grayscale'"><NuxtLink v-bind:to="next_date">⏭️</NuxtLink></div>
       </div>
       <div class="topics-holder w-full mt-10">
         <h1 class="text-xl text-accent-content">Topics Discussed</h1>
